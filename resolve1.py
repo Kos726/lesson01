edef07610875489ac7aa5e1a7be55d8d7f7359f7
Kos726 <konstantin.e.mikhaylov@gmail.com>
2024-06-14 00:41:49 +0300

    Add files via upload

---
+++ resolve1.py
@@ -0,0 +1,28 @@
+# Длина слова
+a = 'Hello, World!'
+print(len(a))
+
+# Суммы и разности
+first = 5
+second = 7
+summa = first + second
+diff = first - second
+print(first, '+', second, '=', summa)
+print(first, '-', second, '=', diff)
+
+# Среднее арифметическое
+third = 12
+mean = (first + second + third) / 3
+print(mean)
+
+# Простые строчки
+first_string = 'Вторник'
+second_string = 'Понедельник'
+print(second_string, ', ', first_string)
+
+# Сложная формула
+x1 = 2
+x2 = 4
+x3 = 9
+f = (((x1 * x2) + (x1 * x3)) ** 3) / 2
+print(f)
